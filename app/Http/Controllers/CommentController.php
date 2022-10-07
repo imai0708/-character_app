79e69f5bf6c93200a0cef7a1583ad08dabdb46cf
imai0708 <asano120pnpn@gmail.com>
2022-10-08 00:59:49 +0900

    コメントコントローラーバリデーションエラー

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,12 +2,11 @@
 
 namespace App\Http\Controllers;
 
-
-use App\Http\Requests\StoreCommentRequest;
-use App\Http\Requests\UpdateCommentRequest;
 use App\Models\Comment;
 use App\Models\Character;
-use Illuminate\Http\Request;
+use App\Http\Requests\CommentRequest;
+use Illuminate\Support\Facades\DB;
+
 
 class CommentController extends Controller
 {
@@ -35,11 +34,9 @@ class CommentController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \App\Http\Requests\CommentRequest  $request
-     * @param  \App\Models\Character  $character
      * @return \Illuminate\Http\Response
      */
-    public function store(CommentRequest $request, Character $character)
-    {
+public function store(CommentReques $request)    {
         $comment = new Comment($request->all());
         $comment->user_id = $request->user()->id;
 
@@ -61,7 +58,7 @@ class CommentController extends Controller
         }
 
         return redirect()
-            ->route('posts.show', $character)
+            ->route('characters.show', $character)
             ->with('notice', 'コメントを登録しました');
     }
 
@@ -78,35 +75,72 @@ class CommentController extends Controller
 
     /**
      * Show the form for editing the specified resource.
-     *
+     * @param  \App\Models\Character  $character
      * @param  \App\Models\Comment  $comment
      * @return \Illuminate\Http\Response
      */
-    public function edit(Comment $comment)
+    public function edit(Character $character, Comment $comment)
     {
-        //
+        return view('comments.edit', compact('character', 'comment'));
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \App\Http\Requests\CommentRequest  $request
+     * @param  \App\Models\Characters  $character
      * @param  \App\Models\Comment  $comment
      * @return \Illuminate\Http\Response
      */
-    public function update(CommentRequest $request, Comment $comment)
-    {
-        //
+public function update(CommentRequest $request, Comment $comment)    {
+        if ($request->user()->cannot('update', $comment)) {
+            return redirect()->route('characters.show', $character)
+                ->withErrors('自分のコメント以外は更新できません');
+        }
+
+        $comment->fill($request->all());
+
+        // トランザクション開始
+        DB::beginTransaction();
+        try {
+            // 更新
+            $comment->save();
+
+            // トランザクション終了(成功)
+            DB::commit();
+        } catch (\Exception $e) {
+            // トランザクション終了(失敗)
+            DB::rollback();
+            return back()->withInput()->withErrors($e->getMessage());
+        }
+
+        return redirect()->route('character.show', $character)
+            ->with('notice', 'コメントを更新しました');
     }
 
     /**
      * Remove the specified resource from storage.
      *
+     * @param  \App\Models\Character  $character
      * @param  \App\Models\Comment  $comment
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Comment $comment)
+    public function destroy(Character $character, Comment $comment)
     {
-        //
+        // トランザクション開始
+        DB::beginTransaction();
+        try {
+            $comment->delete();
+
+            // トランザクション終了(成功)
+            DB::commit();
+        } catch (\Exception $e) {
+            // トランザクション終了(失敗)
+            DB::rollback();
+            return back()->withInput()->withErrors($e->getMessage());
+        }
+
+        return redirect()->route('characters.show', $character)
+            ->with('notice', 'コメントを削除しました');
     }
 }
--- resources/views/characters/show.blade.php
+++ resources/views/characters/show.blade.php
@@ -11,14 +11,28 @@
             </p>
             <img src="{{ Storage::url($character->image_path) }}" alt="" class="mb-4">
             <p class="text-gray-700 text-base">{!! nl2br(e($character->description)) !!}</p>
+            <div class="flex justify-end text-center">
+                @can('update', $comment)
+                    <a href="{{ route('posts.comments.edit', [$post, $comment]) }}"
+                        class="text-sm bg-green-400 hover:bg-green-600 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline w-20 mr-2">編集</a>
+                @endcan
+                @can('delete', $comment)
+                    <form action="{{ route('posts.comments.destroy', [$post, $comment]) }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <input type="submit" value="削除" onclick="if(!confirm('削除しますか？')){return false};"
+                            class="text-sm bg-red-400 hover:bg-red-600 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline w-20">
+                    </form>
+                @endcan
+            </div>
         </article>
 
     </div>
-    
-<div class="flex flex-row text-center my-4">
-    @can('update', $character)
-    <a href="{{ route('characters.edit', $character) }}" @endcan
-        @can('delete', $character)
+
+    <div class="flex flex-row text-center my-4">
+        @can('update', $character)
+        <a href="{{ route('characters.edit', $character) }}" @endcan
+            @can('delete', $character)
         class="bg-purple-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-20 mr-2">編集</a>
     <form action="{{ route('characters.destroy', $character) }}" method="post">
         @csrf
@@ -27,26 +41,26 @@
             class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-20">
     </form>
 @endcan
-        </div>
-
-        @auth
-            <hr class="my-4">
-
-            <div class="flex justify-end">
-                <a href="{{ route('characters.comments.create', $character) }}"
-                    class="bg-indigo-400 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline block">コメント登録</a>
             </div>
-        @endauth
 
-        <section class="font-sans break-normal text-gray-900 ">
-            @foreach ($comments as $comment)
-                <div class="my-2">
-                    <span class="font-bold mr-3">{{ $comment->user->name }}</span>
-                    <span class="text-sm">{{ $comment->created_at }}</span>
-                    <p>{!! nl2br(e($comment->body)) !!}</p>
+            @auth
+                <hr class="my-4">
+
+                <div class="flex justify-end">
+                    <a href="{{ route('characters.comments.create', $character) }}"
+                        class="bg-indigo-400 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline block">コメント登録</a>
                 </div>
-                <hr>
-            @endforeach
-        </section>
-</div>
+            @endauth
+
+            <section class="font-sans break-normal text-gray-900 ">
+                @foreach ($comments as $comment)
+                    <div class="my-2">
+                        <span class="font-bold mr-3">{{ $comment->user->name }}</span>
+                        <span class="text-sm">{{ $comment->created_at }}</span>
+                        <p>{!! nl2br(e($comment->body)) !!}</p>
+                    </div>
+                    <hr>
+                @endforeach
+            </section>
+    </div>
 </x-app-layout>
